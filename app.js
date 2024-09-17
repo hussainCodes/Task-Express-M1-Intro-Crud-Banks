f4d72e86418426a08633956b511349cab0e8ef8d
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-09-17 08:48:22 +0300

    finish tasks and bonus

--- app.js
+++ app.js
@@ -23,6 +23,44 @@ app.post("/accounts", (req, res) => {
   });
 });
 
+app.delete("/accounts/:accountId", (request, response) => {
+  const accountId = request.params.accountId;
+  const wantedAccount = accounts.find((account) => account.id == accountId);
+
+  if (!wantedAccount) {
+    return response.status(404).json({ error: "account not found" });
+  }
+
+  const newAccounts = accounts.filter((account) =>
+    account.id == accountId ? false : true
+  );
+  return response.status(200).json({ data: newAccounts });
+});
+
+app.put("/accounts/:accountId", (request, response) => {
+  const accountId = request.params.accountId;
+  const wantedAccount = accounts.find((account) => account.id == accountId);
+  if (!wantedAccount) {
+    return response.status(404).json({ error: "account not found" });
+  }
+
+  wantedAccount.username = request.body.username;
+
+  return response.status(200).json({ data: wantedAccount });
+});
+
+app.get("/accounts/:username", (request, response) => {
+  const name = request.params.username;
+  const currency = request.query.currency;
+  const wantedAccount = accounts.find((account) => account.username == name);
+  if (currency) {
+    changedCurrency = wantedAccount.funds * 3.2;
+    wantedAccount.funds = changedCurrency;
+  }
+
+  return response.status(200).json({ data: wantedAccount });
+});
+
 app.listen(8000, () => {
   console.log("Hi");
 });
